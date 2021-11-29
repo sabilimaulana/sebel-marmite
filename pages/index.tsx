3f61d378b24c4ffc50a36a3c42d0eab010b3c500
sabilimaulana <spacysbl.learn@gmail.com>
2021-11-29 18:19:09 +0700

    Contentful Client

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,24 @@
-import type { NextPage } from 'next';
+import { createClient } from 'contentful';
+import type { GetStaticProps, NextPage } from 'next';
 
-const Home: NextPage = () => {
+interface HomeProps {
+  recipes: any[];
+}
+
+const Home: NextPage<HomeProps> = ({ recipes }: HomeProps) => {
+  console.log(recipes);
   return <div className="recipe-list">Recipe List</div>;
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID || '',
+    accessToken: process.env.CONTENTFUL_ACCESS_KEY || ''
+  });
+
+  const res = await client.getEntries({ content_type: 'recipe' });
+
+  return { props: { recipes: res.items } };
 };
 
 export default Home;
